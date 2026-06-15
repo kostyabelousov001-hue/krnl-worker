c26609c4debe6872aca8a9bde4717e670e62561e
kostyabelousov001-hue <kostyabelousov001-hue@users.noreply.github.com>
2026-06-15 22:58:51 +0700

    Clean: remove temp files from git

--- start-all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting KRNL server on port 8000...
-start "KRNL Server" cmd /c "set PORT=8000 && node "%~dp0browser-automation\distributed-app.js" --auto --query "real estate Dubai" --passes 3"
-timeout /t 5 /nobreak >nul
-echo Starting Cloudflare tunnel for lol.krnlcamel.space...
-cloudflared tunnel --protocol http2 run krnl-node
-pause
--- start-server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PORT=8000
-node "%~dp0browser-automation\distributed-app.js" %*
